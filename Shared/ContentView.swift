cbd3d53b5146fb2368bb41257f4a6c5cd7116830
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-10-01 01:42:03 +0800

    Attempt to address the preview issues.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -229,8 +229,8 @@ private let itemFormatter: DateFormatter = {
     return formatter
 }()
 
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//
-//    }
-//}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
